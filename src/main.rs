21c7989c0b315aa232007217c13f3e2b17ced61a
Yury Kryvoruchko <django.python4132@gmail.com>
2025-07-07 10:08:50 +0300

    Optimized architecture of components and systems.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use bevy::prelude::*;
 
 #[derive(Component)]
-enum Direction {
-    Left,
-    Right,
-    None
+struct Direction {
+    x: f32,
+    y: f32
 }
+
+#[derive(Component)]
+struct PlayerComponent;
 
 #[derive(Component)]
 struct Speed(f32);
@@ -18,7 +20,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .insert_resource(AsteroidSpawTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
         .add_systems(Startup, startup)
-        .add_systems(Update, (handle_input, move_player, spawn_asteroid).chain())
+        .add_systems(Update, (handle_input, spawn_asteroid, move_objects).chain())
         .run();
 }
 
@@ -29,31 +31,29 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
         Sprite::from_image(asset_server.load("playerShip1_blue.png")),
         Transform::from_xyz(0.0, -400.0, 0.0),
         Speed(150.0),
-        Direction::Left
+        Direction {x: 0.0, y: 0.0},
+        PlayerComponent
     ));
 }
 
-fn handle_input(input: Res<ButtonInput<KeyCode>>, mut directions: Query<&mut Direction>) {
-    for mut direction in &mut directions {
+fn handle_input(input: Res<ButtonInput<KeyCode>>, mut directions: Query<(&mut Direction, &PlayerComponent)>) {
+    for (mut dir, _p) in &mut directions {
         if input.pressed(KeyCode::KeyA) { 
-            *direction = Direction::Left; 
+            dir.x = -1.0;
         }
         else if input.pressed(KeyCode::KeyD) {
-            *direction = Direction::Right;
+            dir.x = 1.0; 
         }
         else {
-            *direction = Direction::None;
+            dir.x = 0.0;
         }
     }
 }
 
-fn move_player(time: Res<Time>, mut player_position: Query<(&mut Direction, &mut Transform, &Speed)>) {
-    for (dir, mut transform, speed) in &mut player_position {
-        match *dir {
-            Direction::Left => transform.translation.x -= speed.0 * time.delta_secs(),
-            Direction::Right => transform.translation.x += speed.0 * time.delta_secs(),
-            _ => (),
-        }
+fn move_objects(time: Res<Time>, mut transforms: Query<(&mut Transform, &Direction, &Speed)>) {
+    for (mut transform, dir, speed) in &mut transforms {
+        transform.translation.x += dir.x * speed.0 * time.delta_secs();
+        transform.translation.y += dir.y * speed.0 * time.delta_secs();
     }
 }
 
@@ -61,7 +61,9 @@ fn spawn_asteroid(time: Res<Time>, mut timer: ResMut<AsteroidSpawTimer>, mut com
     if timer.0.tick(time.delta()).just_finished() {
         commands.spawn((
             Sprite::from_image(asset_server.load("meteorGrey_big3.png")),
-            Transform::from_xyz(rand::random_range(-200.0..=200.0), 0.0, 0.0)
+            Transform::from_xyz(rand::random_range(-200.0..=200.0), 0.0, 0.0),
+            Speed(250.0),
+            Direction {x: 0.0, y: -1.0}
         ));
     }
 }
