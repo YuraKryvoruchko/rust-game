c792a2b810a6ffbdf1259baaf993e4098b83d4b3
Yury Kryvoruchko <django.python4132@gmail.com>
2025-07-08 12:24:23 +0300

    Added shooting.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use bevy::prelude::*;
 
 const PLAYER_SPAWN_HEIGHT: f32 = -400.0;
+const PLAYER_MOVE_SPEED: f32 = 250.0;
 
+const LAZER_SPEED: f32 = 400.0;
+const LAZER_Y_OFFSET: f32 = 40.0;
+const LAZER_LAYER: f32 = -1.0;
+
+const ASTEROID_MOVE_SPEED: f32 = 250.0;
 const ASTEROID_SPAWN_HEIGHT: f32 = 500.0;
 const ASTEROID_SPAWN_DIAPASON: Vec2 = Vec2::new(-200.0, 200.0);
 
@@ -12,20 +18,25 @@ struct Direction {
 }
 
 #[derive(Component)]
-struct PlayerComponent;
+struct Player;
+#[derive(Component)]
+struct Lazer;
 
 #[derive(Component)]
 struct Speed(f32);
 
 #[derive(Resource)]
 struct AsteroidSpawTimer(Timer);
+#[derive(Resource)]
+struct LazerShootingTimer(Timer);
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(AsteroidSpawTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+        .insert_resource(LazerShootingTimer(Timer::from_seconds(0.5, TimerMode::Once)))
         .add_systems(Startup, startup)
-        .add_systems(Update, (handle_input, spawn_asteroid, move_objects).chain())
+        .add_systems(Update, (handle_input, lazer_shooting, spawn_asteroid, move_objects).chain())
         .run();
 }
 
@@ -35,13 +46,13 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
         Sprite::from_image(asset_server.load("playerShip1_blue.png")),
         Transform::from_xyz(0.0, PLAYER_SPAWN_HEIGHT, 0.0),
-        Speed(150.0),
+        Speed(PLAYER_MOVE_SPEED),
         Direction {x: 0.0, y: 0.0},
-        PlayerComponent
+        Player
     ));
 }
 
-fn handle_input(input: Res<ButtonInput<KeyCode>>, mut directions: Query<(&mut Direction, &PlayerComponent)>) {
+fn handle_input(input: Res<ButtonInput<KeyCode>>, mut directions: Query<(&mut Direction, &Player)>) {
     for (mut dir, _p) in &mut directions {
         if input.pressed(KeyCode::KeyA) { 
             dir.x = -1.0;
@@ -62,12 +73,32 @@ fn move_objects(time: Res<Time>, mut transforms: Query<(&mut Transform, &Directi
     }
 }
 
+fn lazer_shooting(time: Res<Time>, mut timer: ResMut<LazerShootingTimer>, input: Res<ButtonInput<KeyCode>>, player: Query<&Transform, With<Player>>, 
+    asset_server: Res<AssetServer>, mut commands: Commands) {
+
+    if !timer.0.tick(time.delta()).finished() {
+        return;
+    }
+
+    if input.just_pressed(KeyCode::Space) {
+        for player_transform in &player {
+            commands.spawn((
+                Sprite::from_image(asset_server.load("laserBlue03.png")),
+                Transform::from_xyz(player_transform.translation.x, player_transform.translation.y + LAZER_Y_OFFSET, LAZER_LAYER),
+                Speed(LAZER_SPEED),
+                Direction {x: 0.0, y: 1.0}
+            ));
+        }
+        timer.0.reset();
+    }
+}
+
 fn spawn_asteroid(time: Res<Time>, mut timer: ResMut<AsteroidSpawTimer>, mut commands: Commands, asset_server: Res<AssetServer>) {
     if timer.0.tick(time.delta()).just_finished() {
         commands.spawn((
             Sprite::from_image(asset_server.load("meteorGrey_big3.png")),
             Transform::from_xyz(rand::random_range(ASTEROID_SPAWN_DIAPASON.x..=ASTEROID_SPAWN_DIAPASON.y), ASTEROID_SPAWN_HEIGHT, 0.0),
-            Speed(250.0),
+            Speed(ASTEROID_MOVE_SPEED),
             Direction {x: 0.0, y: -1.0}
         ));
     }
